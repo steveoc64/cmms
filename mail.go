792b8bb2de3773a64e521e5e3137b2cc659a8979
steveoc64 <steveoc64@gmail.com>
2016-03-22 13:00:50 +1030

    more graceful error handling in mailer - no more panics

--- mail.go
+++ mail.go
@@ -23,8 +23,8 @@ func _initMailer(msg string) {
 	// return
 
 	m := NewMail()
-	m.SetHeader("To", "steve.oconnor@sbsinternational.com.au")
-	// m.SetHeader("To", "steveoc64@gmail.com")
+	m.SetHeader("From", Config.MailSender)
+	m.SetHeader("To", "steveoc64@gmail.com, steve.oconnor@sbsinternational.com.au")
 	m.SetHeader("Subject", "CMMS has started on "+Config.Installation)
 	m.SetBody("text/html", "The CMMS server has been started @"+Config.Installation+":"+Config.MailServer+"\n"+msg)
 	MailChannel <- m
@@ -32,8 +32,15 @@ func _initMailer(msg string) {
 }
 
 func _MailerDaemon() {
-	d := gomail.NewPlainDialer(Config.MailServer, Config.MailPort, Config.MailUser, Config.MailPasswd)
+
+	d := gomail.NewDialer(Config.MailServer, Config.MailPort, Config.MailUser, Config.MailPasswd)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	// d := gomail.Dialer{
+	// 	Host: Config.MailServer,
+	// 	Port: Config.MailPort,
+	// 	SSL:  false,
+	// 	Auth: smtp.PlainAuth("", Config.MailUser, Config.MailPasswd, Config.MailServer),
+	// }
 
 	var s gomail.SendCloser
 	var err error
@@ -46,19 +53,23 @@ func _MailerDaemon() {
 			}
 			if !open {
 				if s, err = d.Dial(); err != nil {
-					panic(err)
+					log.Println("Dialling Error:", err, ",Skipping message:", m.GetHeader("Subject"))
+					return
 				}
 				open = true
 			}
+			m.SetHeader("From", Config.MailSender)
 			if err := gomail.Send(s, m); err != nil {
-				log.Print(err)
+				log.Print("Sending Error:", err)
 			}
 		// Close the connection to the SMTP server if no email was sent in
 		// the last couple of minutes.
 		case <-time.After(120 * time.Second):
 			if open {
 				if err := s.Close(); err != nil {
-					panic(err)
+					log.Println("Inactive Shutdown:", err)
+					return
+
 				}
 				open = false
 			}
